6c81248a9bcc4870f1fa880f8b81cabba69b428f
BogdanPyasetsky <38696453+BogdanPyasetsky@users.noreply.github.com>
2020-12-28 21:16:52 +0200

    Added TextProcessing function

--- MoC2/Program.cs
+++ MoC2/Program.cs
@@ -3,19 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
+using System.IO;
 
 namespace MoC2
 {
     class Program
     {
+        static void TextProcessing()
+        {
+            string RawText = File.ReadAllText("raw_text.txt", Encoding.UTF8);
+            string Text = RawText.ToLower();
+            char[] RestrictedChars = new char[] {'/', '.', ',', '-', '(', ')', '!', '?', '`'};
+            Text = Text.Replace("\n", ""); Text = Text.Replace("\r", "");
+            Text = Text.Replace("ґ", "г"); Text = Text.Replace(" ", "");
+
+            Text = Text.Trim(RestrictedChars);
+
+            Console.WriteLine(Text);
+            File.WriteAllText("pr_text.txt", Text);
+        }
+
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            TextProcessing();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            Console.ReadKey();
         }
     }
 }
